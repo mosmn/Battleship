8c36419c40c5f03ddc1158405dff106475ad4577
mosmn <ithinkisee8@gmail.com>
2023-06-26 14:26:06 +0800

    practice

--- sum.js
+++ sum.js
@@ -2,5 +2,53 @@ function capitalize(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
+function reverseString(string) {
+    return string.split('').reverse().join('');
+}
 
-export default capitalize;
+let calclator = {
+    add: (a, b) => a + b,
+    subtract: (a, b) => a - b,
+    divide: (a, b) => a / b,
+    multiply: (a, b) => a * b
+};
+
+function increment(char) {
+    let charCode = char.charCodeAt(0);
+    if (charCode === 90) {
+        return 'A';
+    } else if (charCode === 122) {
+        return 'a';
+    } else {
+        return String.fromCharCode(charCode + 1);
+    }
+}
+
+function caesarCipher(string, shift) {
+    let cipheredString = '';
+    for (let i = 0; i < string.length; i++) {
+        let char = string[i];
+        if (char.match(/[a-z]/i)) {
+            for (let j = 0; j < shift; j++) {
+                char = increment(char);
+            }
+        }
+        cipheredString += char;
+    }
+    return cipheredString;
+}
+
+function analyzeArray(array) {
+  let average = array.reduce((a, b) => a + b) / array.length;
+  let min = Math.min(...array);
+  let max = Math.max(...array);
+  let length = array.length;
+  return {
+    average: average,
+    min: min,
+    max: max,
+    length: length
+  };
+}
+
+export {capitalize, reverseString, calclator, caesarCipher, analyzeArray};
--- sum.test.js
+++ sum.test.js
@@ -1,6 +1,40 @@
-import {capitalize} from './sum';
+import {capitalize, reverseString, calclator, caesarCipher, analyzeArray} from './sum';
 
 test('Capitalizes the first letter of a string', () => {
     expect(capitalize('hello')).toBe('Hello');
 });
+
+test('Reverses a string', () => {
+    expect(reverseString('hello')).toBe('olleh');
+});
+
+test('Adds 1 + 2 to equal 3', () => {
+    expect(calclator.add(1, 2)).toBe(3);
+});
+
+test('Subtracts 2 - 1 to equal 1', () => {
+    expect(calclator.subtract(2, 1)).toBe(1);
+});
+
+test('Divides 4 / 2 to equal 2', () => {
+    expect(calclator.divide(4, 2)).toBe(2);
+});
+
+test('Multiplies 2 * 2 to equal 4', () => {
+    expect(calclator.multiply(2, 2)).toBe(4);
+});
+
+test('Ciphers "Hello, World!" with a shift of 3 to equal "Khoor, Zruog!"', () => {
+    expect(caesarCipher('Hello, World!', 26)).toBe('Hello, World!');
+});
+
+test('Average, min, max and length of [1,8,3,4,2,6] to equal 4 1 8 6', () => {
+    expect(analyzeArray([1,8,3,4,2,6])).toEqual({
+        average: 4,
+        min: 1,
+        max: 8,
+        length: 6
+    });
+});
+
 
