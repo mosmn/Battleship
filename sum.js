d208a0fe1296591e76f19d31dbf7bd7f44c1b4dc
mosmn <ithinkisee8@gmail.com>
2023-06-26 12:40:29 +0800

    practice

--- sum.js
+++ sum.js
@@ -1,5 +1,6 @@
-function sum(a, b) {
-  return a + b;
+function capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-module.exports = sum;
+
+export default capitalize;
--- sum.test.js
+++ sum.test.js
@@ -1,11 +1,6 @@
-const sum = require('./sum');
+import {capitalize} from './sum';
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+test('Capitalizes the first letter of a string', () => {
+    expect(capitalize('hello')).toBe('Hello');
 });
 
-test('object assignment', () => {
-    const data = {one: 1};
-    data['two'] = 2;
-    expect(data).toEqual({one: 1, two: 2});
-  });
