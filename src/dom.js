e49a4b89355d3b06ee324dfb0eb78a2def22b2f7
mosmn <ithinkisee8@gmail.com>
2023-06-30 06:54:11 +0800

    added createBoard

--- src/dom.js
+++ src/dom.js
@@ -7,4 +7,11 @@ const createElement = (element, className, id) => {
   return newElement;
 }
 
-
+const createBoard = (board, boardId, boardClass) => {
+  const newBoard = createElement("div", boardClass, boardId);
+  for (let i = 0; i < board.length; i++) {
+    const newSquare = createElement("div", "square", `square${i}`);
+    newBoard.appendChild(newSquare);
+  }
+  return newBoard;
+}
